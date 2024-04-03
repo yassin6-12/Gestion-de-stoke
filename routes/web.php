9279ac4cf44457dae82c362deddc71076770fe52
bessaid <zakaryabessaid@yahoo.fr>
2024-04-03 14:20:16 +0100

    l'ajout des tables

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,16 @@ use App\Http\Controllers\ProfileController;
 Route::get('/', function () {
     return view('home');
 });
+//produits
 Route::get('/Produit', [ProduitController::class,'index2'])->name('Produit');
-Route::get('/produit.Liste', [ProduitController::class,'index'])->name('ListeProduit');
-Route::get('/produit.Ajouter',[ProduitController::class,'create'])->name('AjouterProduit');
+Route::get('/produit.Liste', [ProduitController::class,'index'])->name('produit.list');
+Route::get('/produit.Ajouter',[ProduitController::class,'create'])->name('Produit.ajouter');
 // Route::delete('/SupprimerProduit/{id}',[ProduitController::class,'destroy'])->name('SupprimerProduit');
-Route::get('/catégorie.Liste',[CatégorieController::class,'index'])->name('ListeCatégorie');
-Route::get('/catégorie.Ajouter',[CatégorieController::class,'create'])->name('AjouterCatégorie');
+
+// categories
+Route::get('/catégorie.Liste',[CatégorieController::class,'index'])->name('Catégorie.liste');
+Route::get('/catégorie.Ajouter',[CatégorieController::class,'create'])->name('Catégorie.ajouter');
+
+//home
 Route::get('/Profile',[ProfileController::class,'index'])->name('Profile');
 Route::get('/Setting',[ProfileController::class,'update'])->name('Setting');
