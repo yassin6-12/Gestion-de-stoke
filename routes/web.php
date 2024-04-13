0ae2e75031bbfe41b12b3b85328a54aeedfb5307
yassin6-12 <yassineclasse98@gmail.com>
2024-04-13 21:54:44 +0100

    oui

--- routes/web.php
+++ routes/web.php
@@ -12,25 +12,19 @@ Route::get('/', function () {
     return view('home');
 });
 //produits
-Route::get('/Produit', [ProduitController::class,'index2'])->name('Produit');
-Route::get('/produit.Liste', [ProduitController::class,'index'])->name('produit.liste');
-Route::get('/produit.create',[ProduitController::class,'create'])->name('Produit.ajouter');
+Route::get('/Produit.produit', [ProduitController::class,'index2'])->name('Produit');
+Route::get('/produit.index', [ProduitController::class,'index'])->name('produit.index');
+Route::get('/produit.create',[ProduitController::class,'create'])->name('Produit.create');
+Route::post('/produit.store',[ProduitController::class,'store'])->name('Produit.store');
+
 // Route::delete('/SupprimerProduit/{id}',[ProduitController::class,'destroy'])->name('SupprimerProduit');
 
 // categories
 Route::get('/catégorie.index', [CatégorieController::class,'index'])->name('catégorie.index');
 Route::get('/catégorie.create',[CatégorieController::class,'create'])->name('catégorie.create');
-
-// Route::resource('catégorie','Catégorie');
+Route::post('/catégorie.store',[CatégorieController::class,'store'])->name('catégorie.store');
+// Route::resource('catégories',CatégorieController::class);
 
 //home
 Route::get('/Profile',[ProfileController::class,'index'])->name('Profile');
 Route::get('/Setting',[ProfileController::class,'update'])->name('Setting');
-
-//---------------------------------Authentification-----------------------------------
-Route::get('/Authentification.Seconnecter', function () {
-    return view('Authentification.Seconnecter');
-})->name('Seconnecter');
-Route::post('/Authentification.Inscrire', function () {
-    return view('Authentification.Inscrire');
-})->name('S’inscrire');
