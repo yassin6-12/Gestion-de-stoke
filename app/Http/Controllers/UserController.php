1f182dcd1cf43f944213f1ee6dd66f1b134dcf5e
yassin6-12 <yassineclasse98@gmail.com>
2024-05-16 09:28:52 +0100

    Setting/UPDATE photo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
@@ -26,37 +24,46 @@ class UserController extends Controller
     {
         $userId = Auth::id();
         $user = User::findOrFail($userId);
-        // if(request()->has('photo')){
-        //     $imagePath=request()->file('photo')->store('photos','public');
-        //     $validated['photo']=$imagePath;
-        //     Storage::disk('public')->delete($user->image);
-        //     $user->fill([
-        //         'photo' => $request->zipcode
-        //     ]);
-        //     $user->save();
-        //     return redirect()->route('Profile');
-        // }
-        $validated=request()->validate(
-        [
-            'name'=>'required|min:3|max:40',
-            'email'=> 'nullable|min:1|max:255',
+
+        // Validation des données de profil
+        $validated = $request->validate([
+            'name' => 'required|min:3|max:40',
+            'email' => 'nullable|email|max:255',
             'tel' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
             'adresse' => 'required|min:5|max:255',
-            'password' => 'required|min:8',
+            'password' => 'nullable|min:8',
             'city' => 'required|min:3',
             'state' => 'required|min:3',
             'zipcode' => 'required|min:3',
+            'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048', // Validation pour la photo
         ]);
+
+        // Mise à jour de la photo de profil si une nouvelle photo est téléchargée
+        if ($request->hasFile('photo')) {
+            // Supprimer l'ancienne photo si elle existe
+            if ($user->photo) {
+                Storage::disk('public')->delete($user->photo);
+            }
+            // Stocker la nouvelle photo
+            $imagePath = $request->file('photo')->store('photos', 'public');
+            $validated['photo'] = $imagePath;
+        }
+
+        // Mise à jour des autres informations de l'utilisateur
+        if ($request->filled('password')) {
+            $validated['password'] = bcrypt($request->password);
+        } else {
+            unset($validated['password']);
+        }
+
         $user->fill($validated);
         $user->save();
-        return redirect()->route('Profile');
+
+        return response()->json(['success' => 'Profile updated successfully']);
     }
-
-
 
     public function profile()
     {
         return view('Profile');
     }
 }
-
--- resources/views/Setting.blade.php
+++ resources/views/Setting.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.master')
+
 @section('breadcrumb')
     <ol class="breadcrumb">
         <li class="breadcrumb-item">
@@ -9,164 +10,141 @@
         <li class="breadcrumb-item breadcrumb-active" aria-current="page">Paramètres du compte</li>
     </ol>
 @endsection
+
 @section('main')
     <div class="row">
         <div class="col-xxl-8 col-xl-7 col-lg-7 col-md-6 col-sm-12 col-12">
             <div class="row">
                 <div class="col-sm-6 col-12">
                     <div class="d-flex flex-row">
-                        <img src="{{asset('storage/' . Auth::user()->photo)}}" class="img-fluid change-img-avatar" alt="Image">
+                        <img src="{{ asset('storage/' . Auth::user()->photo) }}" class="img-fluid change-img-avatar" alt="Image">
                         <div id="dropzone-sm" class="mb-4 dropzone-dark">
-                            <form action="/upload" class="dropzone needsclick dz-clickable" id="demo-upload">
-
+                            <form action="{{ route('SettingUpdate', $user->id) }}" class="dropzone needsclick dz-clickable" id="myDropzone" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                @method('put')
                                 <div class="dz-message needsclick">
-                                    <button type="button" class="dz-button">Change Image.</button>
+                                    
+                                    <button type="button" class="dz-button">Changer l'image</button>
                                 </div>
-
+                                <div id="produit-other-info" style="display: none"></div>
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
-            <form class="form mt-5" action="{{ route('SettingUpdate',$user->id) }}" method="POST">
-                @csrf
-                @method('put')
+            {{-- <form class="form mt-5" action="{{ route('SettingUpdate', $user->id) }}" method="POST" enctype="multipart/form-data"> --}}
+                <div id="inputSetting">
                 <div class="row">
-                <div class="col-xxl-4 col-sm-6 col-12">
-                    <!-- Form Field Start -->
+                    <div class="col-xxl-4 col-sm-6 col-12">
                         <div class="mb-3">
                             <label for="name" class="form-label">Nom complet</label>
-                            <input type="text" class="form-control" id="name" name="name">
+                            <input type="text" class="form-control" id="name" name="name" value="{{ $user->name }}">
                         </div>
-                </div>
-                <div class="col-xxl-4 col-sm-6 col-12">
-                    <!-- Form Field Start -->
-                    <div class="mb-3">
-                        <label for="email" class="form-label">Email</label>
-                        <input type="email" name="email" class="form-control" id="email">
+                    </div>
+                    <div class="col-xxl-4 col-sm-6 col-12">
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" name="email" class="form-control" id="email" value="{{ $user->email }}">
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-sm-6 col-12">
+                        <div class="mb-3">
+                            <label for="tel" class="form-label">Téléphone</label>
+                            <input type="tel" name="tel" class="form-control" id="tel" value="{{ $user->tel }}">
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-sm-6 col-12">
+                        <div class="mb-3">
+                            <label for="adresse" class="form-label">Adresse</label>
+                            <input type="text" name="adresse" class="form-control" id="adresse" value="{{ $user->adresse }}">
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-sm-6 col-12">
+                        <div class="mb-3">
+                            <label for="city" class="form-label">Ville</label>
+                            <input type="text" name="city" class="form-control" id="city" value="{{ $user->city }}">
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-sm-6 col-12">
+                        <div class="mb-3">
+                            <label for="state" class="form-label">État</label>
+                            <input type="text" name="state" class="form-control" id="state" value="{{ $user->state }}">
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-sm-6 col-12">
+                        <div class="mb-3">
+                            <label for="zipcode" class="form-label">Code postal</label>
+                            <input type="text" name="zipcode" class="form-control" id="zipcode" value="{{ $user->zipcode }}">
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-sm-6 col-12">
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Mot de passe</label>
+                            <input type="password" name="password" class="form-control" id="password" placeholder="Entrer le mot de passe">
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-12">
+                        <hr>
+                        <button type="submit" id="submit-add-produit" class="btn btn-info">Sauvegarder les paramètres</button>
                     </div>
                 </div>
-                <div class="col-xxl-4 col-sm-6 col-12">
-                    <!-- Form Field Start -->
-                    <div class="mb-3">
-                        <label for="tel" class="form-label">Téléphone</label>
-                        <input type="tel" name="tel" class="form-control" id="tel">
-                    </div>
-                </div>
-                <div class="col-xxl-4 col-sm-6 col-12">
-                    <!-- Form Field Start -->
-                    <div class="mb-3">
-                        <label for="adresse" class="form-label">Address</label>
-                        <input type="text" name="adresse" class="form-control" id="adresse">
-                    </div>
-                </div>
-                <div class="col-xxl-4 col-sm-6 col-12">
-                    <!-- Form Field Start -->
-                    <div class="mb-3">
-                        <label for="city" class="form-label">City</label>
-                        <input type="text" name="city" class="form-control" id="city">
-                    </div>
-                </div>
-                <div class="col-xxl-4 col-sm-6 col-12">
-                    <!-- Form Field Start -->
-                    <div class="mb-3">
-                        <label for="state" class="form-label">State</label>
-                        <input type="text" name="state" class="form-control" id="state">
-                    </div>
-                </div>
-                <div class="col-xxl-4 col-sm-6 col-12">
-                    <!-- Form Field Start -->
-                    <div class="mb-3">
-                        <label for="zipcode" class="form-label">Code postal</label>
-                        <input type="text" name="zipcode" class="form-control" id="zipcode">
-                    </div>
-                </div>
-
-                <div class="col-xxl-4 col-sm-6 col-12">
-                    <!-- Form Field Start -->
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input type="password" name="password" class="form-control" id="password" placeholder="Enter Password">
-                    </div>
-                </div>
-                <div class="col-sm-12 col-12">
-                    <hr>
-                    <button type="submit" class="btn btn-info">Save Settings</button>
-                </div>
-                </div>
-            </form>
+            </div>
+            {{-- </form> --}}
         </div>
     </div>
     <!-- Dropzone JS -->
-		<script src="assets/vendor/dropzone/dropzone.min.js"></script>
+    <script src="assets/vendor/dropzone/dropzone.min.js"></script>
+    <script type="text/javascript">
+        Dropzone.autoDiscover = false;
+        var maxFilesizeVal = 12;
+        var maxFilesVal = 10;
+        var myDropzone = new Dropzone("#myDropzone", {
+            url: "{{ route('SettingUpdate', $user->id) }}", // URL d'upload
+            autoProcessQueue: false, // Désactiver l'auto-upload
+            method: 'POST',
+            paramName: 'photo',
+            maxFilesize: maxFilesizeVal,
+            maxFiles: 1,
+            parallelUploads: maxFilesVal,
+            resizeQuality: 1.0,
+            acceptedFiles: ".jpeg,.jpg,.png,.webp",
+            addRemoveLinks: true,
+            timeout: 60000,
+            dictDefaultMessage: "Déposez vos fichiers ici ou cliquez pour télécharger",
+            dictFallbackMessage: "Votre navigateur ne supporte pas le téléchargement par glisser-déposer.",
+            dictFileTooBig: "Le fichier est trop volumineux. Taille max : " + maxFilesizeVal + "Mo.",
+            dictInvalidFileType: 'Type de fichier invalide. Seuls les fichiers jpg, jpeg, png et webp sont autorisés.',
+            dictMaxFilesExceeded: 'Vous ne pouvez télécharger qu\'un seul fichier.',
+            maxfilesexceeded: function(file) {
+                this.removeFile(file);
+            },
+            sending: function(file, xhr, formData) {
+                formData.append("_token", "{{ csrf_token() }}");
+                formData.append("_method", "PUT");
+                // Ajoutez d'autres champs de formulaire si nécessaire
+            },
+            success: function(file, response) {
+                $('#message').text(response.message);
+                console.log(response.success);
+            },
+            error: function(file, response) {
+                $('#message').text('Une erreur est survenue ! ' + response);
+                console.log(response);
+                return false;
+            },
+        });
+
+        function submitForm() {
+            var form = document.getElementById('myDropzone');
+            form.submit();
+        }
+
+        document.getElementById("submit-add-produit").addEventListener("click", function(e) {
+            e.preventDefault();inputSetting
+            e.stopPropagation();
+            $('#produit-other-info').append($('#inputSetting'));
+            myDropzone.processQueue(); // Déclencher l'upload des fichiers
+            submitForm();
+        });
+    </script>
 @endsection
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                <!-- <div class="settings-block">
-                        <div class="settings-block-title">Other Settings</div>
-                        <div class="settings-block-body">
-                            <div class="list-group">
-                                <div class="list-group-item d-flex justify-content-between align-items-center">
-                                    <div>Show desktop notifications</div>
-                                    <div class="form-check form-switch">
-                                        <input class="form-check-input" type="checkbox" id="showNotifications">
-                                        <label class="form-check-label" for="showNotifications"></label>
-                                    </div>
-                                </div>
-                                <div class="list-group-item d-flex justify-content-between align-items-center">
-                                    <div>Show email notifications</div>
-                                    <div class="form-check form-switch">
-                                        <input class="form-check-input" type="checkbox" id="showEmailNotifications" checked>
-                                        <label class="form-check-label" for="showEmailNotifications"></label>
-                                    </div>
-                                </div>
-                                <div class="list-group-item d-flex justify-content-between align-items-center">
-                                    <div>Show chat notifications</div>
-                                    <div class="form-check form-switch">
-                                        <input class="form-check-input" type="checkbox" id="showChatNotifications">
-                                        <label class="form-check-label" for="showChatNotifications"></label>
-                                    </div>
-                                </div>
-                                <div class="list-group-item d-flex justify-content-between align-items-center">
-                                    <div>Show purchase history</div>
-                                    <div class="form-check form-switch">
-                                        <input class="form-check-input" type="checkbox" id="showPurchaseNotifications">
-                                        <label class="form-check-label" for="showPurchaseNotifications"></label>
-                                    </div>
-                                </div>
-                                <div class="list-group-item d-flex justify-content-between align-items-center">
-                                    <div>Show orders</div>
-                                    <div class="form-check form-switch">
-                                        <input class="form-check-input" type="checkbox" id="showOrders">
-                                        <label class="form-check-label" for="showOrders"></label>
-                                    </div>
-                                </div>
-                                <div class="list-group-item d-flex justify-content-between align-items-center">
-                                    <div>Show alerts</div>
-                                    <div class="form-check form-switch">
-                                        <input class="form-check-input" type="checkbox" id="showAlerts">
-                                        <label class="form-check-label" for="showAlerts"></label>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
-
-                     {{-- <div class="col-xxl-4 col-lg-5 col-md-6 col-sm-12 col-12">
-            <div class="account-settings-block">
-
-
-            </div>
-        </div> --}}
