2c1ec924656281c6f136a72ebf08c48577a2f785
yassin6-12 <yassineclasse98@gmail.com>
2024-06-02 02:00:46 +0100

    modification de style

--- resources/views/admin/produit/Panier.blade.php
+++ resources/views/admin/produit/Panier.blade.php
@@ -9,13 +9,21 @@
     </ol>
 @endsection
 @section('main')
+<style>
+      .product-image {
+        max-width: 100%;
+        height: auto;
+        max-height: 300px; /* Adjust the max-height as needed */
+        object-fit: cover; /* Ensures the image covers the box and maintains aspect ratio */
+    }
+</style>
 <div class="row">
     <div class="col-sm-12 col-12">
         <div class="card">
-            <div class="card-header">
+            <div class="card-header d-flex justify-content-between align-items-center">
                 <div class="card-title">Éléments ajoutés</div>
                 <div class="ml-auto">
-                    <a href="{{route('produit.index')}}" class="btn btn-dark">Retour aux produits</a>
+                    <a href="{{ route('produit.index') }}" class="btn btn-dark">Retour aux produits</a>
                 </div>
             </div>
             <div class="card-body">
@@ -28,23 +36,22 @@
                                 @php
                                     $firstImage = json_decode($produit->images)[0];
                                 @endphp
-                                <img src="{{asset('storage/' .$firstImage)}}" alt="Admin Templates">
+                                <img src="{{ asset('storage/' . $firstImage) }}" alt="Admin Templates" class="img-fluid product-image">
                                 <div class="product-added-card-body">
-                                    <h5 class="product-added-title">{{$produit->nom}}</h5>
-                                    <div class="product-added-price">DA<span class="product-prix-unitaire">{{$produit->prix - ($produit->prix * $produit->remise/100)}}</span>
+                                    <h5 class="product-added-title">{{ $produit->nom }}</h5>
+                                    <div class="product-added-price">DA<span class="product-prix-unitaire">{{ $produit->prix - ($produit->prix * $produit->remise / 100) }}</span>
                                     </div>
                                     <div class="product-added-description">
-                                        {{$produit->description}}
+                                        {{ $produit->description }}
                                     </div>
                                     <div class="product-added-actions">
-                                        <button class="btn btn-light remove-from-cart">Retirer de panier</button>
+                                        <button class="btn btn-light remove-from-cart">Retirer du panier</button>
                                     </div>
-                                    <div class="produtct-added-quantity my-2">
+                                    <div class="product-added-quantity my-2">
                                         <div class="d-flex align-items-center">
-                                            <label for="quantity" class="label-control fw-bold me-3">quantity: </label>
-                                            <input type="number" name="quantity-{{$produit->id}}" id="{{$produit->id}}"  value="1" min="1" max="{{$produit->qte_stock}}"  placeholder="quantity" class="form-control quantity" />
+                                            <label for="quantity" class="label-control fw-bold me-3">Quantité: </label>
+                                            <input type="number" name="quantity-{{ $produit->id }}" id="{{ $produit->id }}" value="1" min="1" max="{{ $produit->qte_stock }}" placeholder="Quantité" class="form-control quantity" />
                                         </div>
-                                        
                                     </div>
                                 </div>
                             </div>
@@ -52,11 +59,10 @@
                     @endforeach
                     <div class="col-sm-12 col-12">
                         <div class="sub-total-container">
-                            <div class="total" >Order Total: DA<span id="total-panier"></span></div>
-                            
-                            <form action="{{route('facture')}}" method="POST" id="form-panier-to-facture">
+                            <div class="total">Total de la commande: DA<span id="total-panier"></span></div>
+                            <form action="{{ route('facture') }}" method="POST" id="form-panier-to-facture">
                                 @csrf
-                                <button type="submit"  id="submit-panier" class="btn btn-success btn-lg">Caisse</button>
+                                <button type="submit" id="submit-panier" class="btn btn-success btn-lg">Caisse</button>
                             </form>
                         </div>
                     </div>
@@ -70,55 +76,48 @@
 @endsection
 @section('script')
     <script>
-        
         $(document).ready(function(){
-            // set the first total price of all products (panier)
-
+            // Calculer le prix total initial des produits dans le panier
             var total = 0;
-            $('.product-prix-unitaire').each(function(index,element){
+            $('.product-prix-unitaire').each(function(){
                 total += parseFloat($(this).text());
-            })
-            $('#total-panier').text(total)
+            });
+            $('#total-panier').text(total);
 
-            // change price when change the quantity
-
-            $('input.quantity').keyup(function(){
-                if(parseInt($(this).val())  > parseInt($(this).attr('max'))){
+            // Mettre à jour le prix total lorsque la quantité change
+            $('input.quantity').on('input change', function(){
+                if (parseInt($(this).val()) > parseInt($(this).attr('max'))) {
                     $(this).val($(this).attr('max'));
                 }
-
-                if($(this).val() != '' && $(this).val() > 0){
+                if ($(this).val() !== '' && $(this).val() > 0) {
                     total = 0;
-                    $('.product-prix-unitaire').each(function(index,element){
-                        let quantity = $(this).parent().siblings('.produtct-added-quantity').find('.quantity').val();
+                    $('.product-prix-unitaire').each(function(){
+                        let quantity = $(this).closest('.product-added-card-body').find('.quantity').val();
                         total += (parseFloat($(this).text()) * quantity);
-                    })
-                    $('#total-panier').text(total)
+                    });
+                    $('#total-panier').text(total);
                 }
-                
-            })
-            $('input.quantity').change(function(){
-                if($(this).val() != '' && $(this).val() > 0){
-                    total = 0;
-                    $('.product-prix-unitaire').each(function(index,element){
-                        let quantity = $(this).parent().siblings('.produtct-added-quantity').find('.quantity').val();
-                        total += (parseFloat($(this).text()) * quantity);
-                    })
-                    $('#total-panier').text(total)
-                }
-            })
+            });
 
-            // add quantity of each product to the form before go to facture page
+            // Ajouter la quantité de chaque produit au formulaire avant de soumettre
             $('#submit-panier').click(function(e){
                 var quantities = [];
                 var products = [];
-                $('input.quantity').each(function(index,ele){
+                $('input.quantity').each(function(){
                     quantities.push($(this).val());
                     products.push($(this).attr('id'));
-                })
-                $(this).before('<input type="hidden" name="products" value='+JSON.stringify(products)+' />');
-                $(this).before('<input type="hidden" name="quantities" value='+JSON.stringify(quantities)+' />');
-            })
-        })
+                });
+                $('<input>').attr({
+                    type: 'hidden',
+                    name: 'products',
+                    value: JSON.stringify(products)
+                }).appendTo('#form-panier-to-facture');
+                $('<input>').attr({
+                    type: 'hidden',
+                    name: 'quantities',
+                    value: JSON.stringify(quantities)
+                }).appendTo('#form-panier-to-facture');
+            });
+        });
     </script>
 @endsection
